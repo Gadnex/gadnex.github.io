be1db15f9da24e7d5d4588bc71d376f56d000cf1
William Gadney <william.gadney@mendesgans.nl>
2025-06-05 14:24:49 +0200

    Change code highlight to use Shiki

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
-module.exports = function (eleventyConfig) {
+import MarkdownIt from "markdown-it";
+import Shiki from "@shikijs/markdown-it";
+
+export default async function (eleventyConfig) {
 
     eleventyConfig.setInputDirectory("src");
 
@@ -12,4 +15,23 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/*.css");
     eleventyConfig.addPassthroughCopy("src/*.js");
+
+    const options = {
+        html: true,
+        breaks: true,
+        linkify: true,
+    };
+
+    let markdownLib = MarkdownIt(options)
+      .use(
+        await Shiki({
+            // theme: "rose-pine-moon",
+            themes: {
+                light: 'min-light',
+                dark: 'nord',
+            }
+        }),
+      );
+
+    eleventyConfig.setLibrary("md", markdownLib);
 };
